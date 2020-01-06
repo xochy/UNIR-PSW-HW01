5a0fed9f8dfda5277b5ecf968b010a39b6586e31
xochy <xochydeather@gmail.com>
2020-01-05 19:07:54 -0600

    Se agregan comentarios al código y se elimina el archivo contenido.php

--- index.php
+++ index.php
@@ -12,36 +12,42 @@ session_start();
 
 <body>
   <?php
-  //* 1. Si acaban de enviar el formulario de acceso, leemos de $_POST los datos:
+  //* Si acaban de enviar el formulario de acceso, leemos de $_POST los datos:
   if (isset($_POST["usuario"]) and isset($_POST["clave"])) {
 
-    //* 2. En ese caso, verificamos que no estén vacíos:
+    //* En ese caso, verificamos que no estén vacíos:
     if ($_POST["usuario"] == "" or $_POST["clave"] == "") {
 
       print("<p>Por favor, completar los campos de usuario y clave</p>");
-
     } else {
 
+      //* Leemos el archivo entero "suscriptores.txt" y su contenido (cada línea) se almacena en un arreglo:
       $usuarios     = file("suscriptores.txt");
+      //* Definimos el arreglo que almacena las credenciales de los usuarios:
       $credenciales = array();
 
+      //* Utilizamos el bucle "foreach" para recorrer la matriz que contiene la información de los usuarios:
       foreach ($usuarios as $usuarioArchivo) {
 
+        //* Verificamos que el usuario (que representa a cada línea del archivo de texto) contenga información:
         if (empty(preg_replace('~[\r\n]+~', '', $usuarioArchivo))) continue;
 
+        //* Creamos un nuevo arreglo que almacena la información correspondiente al usuario y contraseña:
         $usuarioArr = explode('-', $usuarioArchivo);
 
+        //* Almacenamos el usuario (key) con su respectiva contraseña (value):
         $credenciales[$usuarioArr[0]] = $usuarioArr[1];
       }
 
+      //* Verificamos que el usuario que intenta ingresar al sistema haya introducido las credenciales correctas comparando con la información que contiene
+      //* el arreglo "$credenciales", pues éste contiene la información de lo usuarios registrados:
       if (array_key_exists($_POST["usuario"], $credenciales) && preg_replace('~[\r\n]+~', '', $credenciales[$_POST["usuario"]]) == sha1($_POST["clave"])) {
 
-        //* 4. Si eran correctos los datos, los colocamos en variables de sesión:
+        //* Si eran correctos los datos, los colocamos en variables de sesión:
         $_SESSION["usuario"] = $_POST["usuario"];
         $_SESSION["clave"]   = $_POST["clave"];
 
         print("<p>Bienvenido, usted se ha identificado correctamente como: " . $_SESSION["usuario"] . "</p>");
-        
       } else {
 
         print("<p>¡Usuario y/o contraseña incorrecta, favor de revisar sus datos!</p>");
@@ -69,6 +75,7 @@ session_start();
     <div class="container">
       <span class="psw">¿Eres nuevo?<a href="registro.php">¡Regístrate!</a></span>
       <?php
+      //* Verificamos que la sesión del usuario exista, y de ser así, mostrar la opción para cerrar la sesión actual:
       if (isset($_SESSION["usuario"]) and $_SESSION["usuario"] <> "") { ?>
         <a href="logout.php">Cerrar sesión</a>
       <?php
--- registro.php
+++ registro.php
@@ -5,34 +5,35 @@ session_start();
 <html>
 
 <head>
-    <title>Zona de Cierre de Sesión</title>
+    <title>Zona de Registro</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="estilo.css">
 </head>
 
 <body>
     <?php
-    //* 1. Si acaban de enviar el formulario de acceso, leemos de $_POST los datos:
+    //* Si acaban de enviar el formulario de acceso, leemos de $_POST los datos:
     if (isset($_POST["usuario"]) and isset($_POST["clave"])) {
 
-        //* 2. En ese caso, verificamos que no estén vacíos:
+        //* En ese caso, verificamos que no estén vacíos:
         if ($_POST["usuario"] == "" or $_POST["clave"] == "") {
 
             print("<p>Por favor, completar los campos de usuario y clave</p>");
         } else {
 
-            //* Abrimos el archivo, pero esta vez para añadir.
+            //* Abrimos el archivo, pero esta vez para añadir información:
             $archivoAbierto = fopen("suscriptores.txt", "a");
 
-            //* Preparamos los datos
+            //* Preparamos los datos:
             $usuario = $_POST["usuario"];
-            $clave = $_POST["clave"];
-            $texto = $usuario . "-" . sha1($clave) . "\n";
+            $clave   = $_POST["clave"];
+            $texto   = $usuario . "-" . sha1($clave) . "\n";
 
-            //* Intentamos añadirlos, validando que se haya podido hacer
+            //* Intentamos añadirlos, validando que se haya podido hacer:
             if (fputs($archivoAbierto, $texto)) {
 
                 print("<p>¡Gracias por sus datos, el registro se ha realizado exitosamente!</p>");
+
             } else {
 
                 print("<p>Hubo un error. Intente nuevamente</p>");
@@ -57,9 +58,7 @@ session_start();
         </form>
     </div>
     <div id="menu">
-        <ul>
-            <li><a href="index.php">Página de Ingreso</a></li>
-        </ul>
+        <a href="index.php">Página de Ingreso</a>
     </div>
 </body>
 </html>
